bc84c5fd1afec2a9fc852b8f55341c36f0aecdb5
malcomowilla <owillamalcom@gmail.com>
2023-06-24 10:51:48 +0300

    solved

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,5 @@
 class BirdsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   # GET /birds
   def index
     birds = Bird.all
@@ -14,23 +14,19 @@ class BirdsController < ApplicationController
 
   # GET /birds/:id
   def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
+    bird = find_bird
       render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    
+    
   end
 
   # PATCH /birds/:id
   def update
-    bird = Bird.find_by(id: params[:id])
-    if bird
+    bird = find_bird
       bird.update(bird_params)
       render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    
+    
   end
 
   # PATCH /birds/:id/like
@@ -56,6 +52,15 @@ class BirdsController < ApplicationController
   end
 
   private
+def render_not_found_response
+  render json: {error: "Bird not found"}, status: :not_found
+  
+end
+
+def find_bird
+  Bird.find(params[:id])
+end
+  
 
   def bird_params
     params.permit(:name, :species, :likes)
--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,13 @@ Rails.application.routes.draw do
   resources :birds
   patch "/birds/:id/like", to: "birds#increment_likes"
 end
+
+
+
+
+
+
+
+
+
+
